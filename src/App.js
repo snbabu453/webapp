96a8055d7ce97d35b365a2486f3507933ff07e97
snbabu453 <snbabu453@gmail.com>
2020-02-04 17:46:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>This is form master branch - app test</p>
+        <a
+          className="App-link"
+          href="http://www.imaginnovate.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here to navigate to imaginnovate website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
