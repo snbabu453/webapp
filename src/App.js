7a63cdf4bbda1dc301ea2cdf1d2a11bf2e7d23b0
snbabu453 <snbabu453@gmail.com>
2020-02-05 10:02:35 +0530

    u

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>This is form master branch - app test</p>
+        <p>This is form master branch - app test updated</p>
         <a
           className="App-link"
           href="http://www.imaginnovate.com/"
